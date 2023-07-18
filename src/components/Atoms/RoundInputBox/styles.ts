2e00e4588edb38dfe77c4ca33e85f7109e30695d
seyeon <yscat618@gmail.com>
2023-07-18 22:42:10 +0900

    input box style 수정

--- src/components/Atoms/RoundInputBox/styles.ts
+++ src/components/Atoms/RoundInputBox/styles.ts
@@ -2,15 +2,15 @@ import { styled } from "@mui/material/styles";
 import TextField from "@mui/material/TextField";
 
 export const Input = styled(TextField)`
-  .css-9ddj71-MuiInputBase-root-MuiOutlinedInput-root {
+  .MuiInputBase-root-MuiOutlinedInput-root {
     height: 42px;
   }
 
-  .css-1d3z3hw-MuiOutlinedInput-notchedOutline {
+  .MuiOutlinedInput-notchedOutline {
     border-color: #713edc !important;
   }
 
-  .css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input {
+  .MuiInputBase-root {
     font-family: Pretendard, sans-serif;
     font-weight: 400;
     font-size: 10px;
