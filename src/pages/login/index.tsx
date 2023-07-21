e520cba97a8ab530e52fe45312f7dc87c04f52ef
seyeon <yscat618@gmail.com>
2023-07-21 11:16:09 +0900

    로그인 유효성 검사 추가, 백엔드 연결

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useRouter } from "next/router";
+import axios from "axios";
 
+import ErrorMsg from "@/components/Atoms/ErrorMsg";
 import IdeationLabel from "@/components/Atoms/IdeationLabel";
 import InputBox from "@/components/Atoms/InputBox";
 import KakaoButton from "@/components/Atoms/KakaoButton";
@@ -12,13 +14,28 @@ import { Container, FlexWrap, InnerContainer } from "@/styles/login/styles";
 import TitleCard from "../../components/Templates/Card";
 
 function Login(): React.ReactElement {
-  const [id, setId] = useState("");
+  const router = useRouter();
+
+  const [email, setEmail] = useState("");
   const [pw, setPw] = useState("");
   const [autoLogin, setAutoLogin] = useState(false);
-  const router = useRouter();
+  const [err, setErr] = useState("");
 
   const handleAutoLogin = () => {
     setAutoLogin(!autoLogin);
+  };
+
+  const login = () => {
+    const data = {
+      email: email,
+      password: pw,
+    };
+    axios
+      .post(`${process.env.NEXT_PUBLIC_BASEURL}/auth/login`, data)
+      .then((res) => {
+        if (res.data.error) setErr("이메일 또는 비밀번호를 잘못 입력했습니다.");
+        else router.push("/");
+      });
   };
 
   return (
@@ -29,9 +46,9 @@ function Login(): React.ReactElement {
           <FlexWrap gap={20}>
             <FlexWrap gap={25}>
               <InputBox
-                placeHolder={"아이디를 입력하세요"}
-                text={id}
-                setText={setId}
+                placeHolder={"이메일을 입력하세요"}
+                text={email}
+                setText={setEmail}
               />
               <PasswordInputBox
                 placeHolder={"비밀번호를 입력하세요"}
@@ -44,8 +61,9 @@ function Login(): React.ReactElement {
               setCheck={handleAutoLogin}
               text={"자동 로그인"}
             />
+            {err && <ErrorMsg errText={err} />}
             <FlexWrap gap={6}>
-              <RoundButton isFilled={true} text={"로그인"} />
+              <RoundButton isFilled={true} text={"로그인"} onClick={login} />
               <RoundButton
                 isFilled={false}
                 text={"회원가입"}
