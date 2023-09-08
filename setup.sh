2302dece6ff1fc95a280b8aa7941a8646c8f0834
luna <luna.s.jo@polarisoffice.com>
2023-09-08 10:31:27 +0900

    오타 수정

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ cd zlib-1.2.9
 sh configure
 make
 cp libz.so.1.2.9 ../node_modules/canvas/build/Release/libz.so.X
-cd..
+cd ..
 
 patchelf --replace-needed /lib64/libz.so.1 libz.so.X ./node_modules/canvas/build/Release/libpng16.so.16
 patchelf --replace-needed libz.so.1 libz.so.X ./node_modules/canvas/build/Release/libpng16.so.16
