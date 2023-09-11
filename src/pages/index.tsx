7bfcf4c317dd3357bbdbc348fbf9e79a459cbf8c
seyeon <yscat618@gmail.com>
2023-09-11 23:44:24 +0900

    root 경로 진입 시 main 페이지로 이동

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,12 @@
-import React, { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/router";
 
-import { useAuth } from "@/utils/auth";
-
-function Root(): React.ReactElement {
+function Root(): void {
   const router = useRouter();
-  const { authLogout, isLoggedIn } = useAuth();
-
-  const [text, setText] = useState("");
 
   useEffect(() => {
-    const checkLoginStatus = async () => {
-      if (isLoggedIn()) {
-        setText("로그아웃");
-      } else {
-        setText("로그인");
-      }
-    };
-
-    checkLoginStatus();
-  }, [isLoggedIn]);
-
-  const handleClick = () => {
-    if (isLoggedIn()) {
-      authLogout();
-    } else {
-      setText("로그아웃");
-      router.push("/login");
-    }
-  };
-
-  return (
-    <>
-      <div>
-        {"Ideation 화이팅!!! (아래 못생긴건 테스트를 위한 임시 버튼임..)"}
-      </div>
-      <button onClick={handleClick}>{text}</button>
-    </>
-  );
+    router.push("/main");
+  }, []);
 }
 
 export default Root;
