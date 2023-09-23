4cc4dfdeb5e9c326605296a603f1f5523affc0d4
seyeon <yscat618@gmail.com>
2023-09-23 23:15:57 +0900

    ai: idea 늘려쓰기 구현 완료

--- src/components/Atoms/TextArea/index.tsx
+++ src/components/Atoms/TextArea/index.tsx
@@ -1,6 +1,8 @@
-import React, { Dispatch, SetStateAction } from "react";
+import React, { Dispatch, SetStateAction, useRef, useState } from "react";
 
-import { StyledTextArea } from "./styles";
+import { useAuth } from "@/utils/auth";
+
+import { ContextItem, ContextMenu, Progress, StyledTextArea } from "./styles";
 
 function TextArea({
   placeholder,
@@ -11,14 +13,78 @@ function TextArea({
   text: string;
   setText: Dispatch<SetStateAction<string>>;
 }): React.ReactElement {
+  const { axios } = useAuth();
+
+  const [showContextMenu, setShowContextMenu] = useState(false);
+  const [position, setPosition] = useState({ top: 0, left: 0 });
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  const [dragText, setDragText] = useState<string>("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSelect = (e: React.MouseEvent<HTMLTextAreaElement>) => {
+    if (textAreaRef.current) {
+      const { selectionStart, selectionEnd } = textAreaRef.current;
+      if (selectionStart !== selectionEnd) {
+        setShowContextMenu(true);
+        setPosition({
+          top: e.nativeEvent.y,
+          left: e.nativeEvent.x,
+        });
+        const draggedText = textAreaRef.current.value.substring(
+          selectionStart,
+          selectionEnd
+        );
+        setDragText(draggedText);
+      } else {
+        setShowContextMenu(false);
+      }
+    }
+  };
+
+  const getStretchedIdea = () => {
+    setLoading(true);
+    const data = {
+      ideaName: dragText,
+    };
+    axios
+      .post(`${process.env.NEXT_PUBLIC_BASEURL}/idea/ai/stretch`, data)
+      .then((res) => {
+        setText(res.data.data.content);
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
   return (
-    <StyledTextArea
-      placeholder={placeholder}
-      value={text}
-      onChange={(e) => {
-        setText(e.target.value);
-      }}
-    />
+    <>
+      {loading && <Progress color="secondary" />}
+      <StyledTextArea
+        ref={textAreaRef}
+        placeholder={placeholder}
+        value={text}
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
+        onSelect={handleSelect}
+        disabled={loading}
+      />
+      {showContextMenu && (
+        <ContextMenu top={position.top} left={position.left}>
+          <ContextItem
+            onClick={() => {
+              setShowContextMenu(false);
+              getStretchedIdea();
+            }}
+          >
+            {"AI로 늘려쓰기"}
+          </ContextItem>
+        </ContextMenu>
+      )}
+    </>
   );
 }
 
--- src/components/Atoms/TextArea/styles.ts
+++ src/components/Atoms/TextArea/styles.ts
@@ -1,4 +1,5 @@
 import { TextareaAutosize } from "@mui/base";
+import CircularProgress from "@mui/material/CircularProgress";
 import { styled } from "@mui/material/styles";
 
 export const StyledTextArea = styled(TextareaAutosize)`
@@ -18,5 +19,34 @@ export const StyledTextArea = styled(TextareaAutosize)`
     outline: none;
     border: 2px solid #7b40f2;
     box-shadow: none;
+  }
+  &:disabled {
+    background-color: gainsboro;
+  }
+`;
+
+export const ContextMenu = styled("div")<{ top: number; left: number }>`
+  position: absolute;
+  top: ${(props) => props.top}px;
+  left: ${(props) => props.left}px;
+  z-index: 9999;
+  border-radius: 8px;
+  background: var(--White, #fff);
+  box-shadow: 0px 2px 15px -3px rgba(0, 0, 0, 0.07);
+`;
+
+export const ContextItem = styled("div")`
+  padding: 10px;
+  border-radius: 8px;
+  cursor: pointer;
+
+  :hover {
+    background: rgba(0, 0, 0, 0.1);
+  }
+`;
+
+export const Progress = styled(CircularProgress)`
+  &.MuiCircularProgress-root {
+    position: absolute;
   }
 `;
