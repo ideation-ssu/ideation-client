8c3cbd52806af83df0f98807aabbc679a5e794d9
seyeon <yscat618@gmail.com>
2023-09-24 20:05:44 +0900

    배포 에러 수정

--- public/icons/Comment/styles.ts.tsx
+++ public/icons/Comment/styles.ts.tsx
@@ -1,6 +1,6 @@
 import { styled } from "@mui/material/styles";
 
-export const CommentIcon = styled("div")<{ isLiked: boolean }>`
+export const CommentIcon = styled("div")<{ isLiked?: boolean }>`
   background-image: ${(props) =>
     props.isLiked
       ? `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='14' viewBox='0 0 16 14' fill='none'%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M13.4141 0C14.5186 0 15.4141 0.895431 15.4141 2V8.57404C15.4141 9.67861 14.5186 10.574 13.4141 10.574H10.0182V12.7388C10.0182 13.1401 9.56915 13.3779 9.23719 13.1524L5.44272 10.574H1.99697C0.892406 10.574 -0.00302505 9.67861 -0.00302505 8.57404V2C-0.00302505 0.895431 0.892406 0 1.99698 0H13.4141Z' fill='%237B40F2'/%3E%3C/svg%3E")`
